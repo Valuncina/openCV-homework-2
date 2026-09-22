9c16b02ea0aab5a173fdf291168226608b473c63
Valuncina <litinghao54188@163.com>
2026-09-22 22:47:49 +0800

    Version: 0.0.3

--- opencv_1.cpp
+++ opencv_1.cpp
@@ -4,7 +4,7 @@
 using namespace cv;
 
 int main() {
-    Mat img = imread("image_0.png");
+    Mat img = imread("resources/image_0.png");
     if (img.empty()) {
         std::cerr << "Cannot read image! 请检查图片路径\n";
         return 1;
@@ -26,18 +26,14 @@ int main() {
     waitKey(0);
 
 
-    Mat meanImg, gaussianImg, medianImg;
-    namedWindow("Mean", WINDOW_NORMAL);
-    namedWindow("Gaussian", WINDOW_NORMAL);
-    namedWindow("Median", WINDOW_NORMAL);
-    blur(img, meanImg, Size(5, 5));
-    GaussianBlur(img, gaussianImg, Size(5, 5), 1.5);
-    medianBlur(img, medianImg, 5);
-    imshow("Mean", meanImg);
-    waitKey(0);
-    imshow("Gaussian", gaussianImg);
-    waitKey(0);
-    imshow("Median", medianImg);
+    Mat binary , adaptive;
+    threshold(gray, binary, 128, 255, THRESH_BINARY);
+    adaptiveThreshold(gray,adaptive,255,ADAPTIVE_THRESH_GAUSSIAN_C,THRESH_BINARY,11,2);
+
+    namedWindow("Binary",WINDOW_NORMAL);
+    imshow("Binary",binary);
+    namedWindow("Adaptive",WINDOW_NORMAL);
+    imshow("Adaptive",adaptive);
     waitKey(0);
     
     destroyAllWindows(); // 主动释放所有窗口资源，比直接点叉优雅
